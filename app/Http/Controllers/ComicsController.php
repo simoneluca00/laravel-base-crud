22277925a359280627018276f80695e564477687
simoneluca00 <si.zaino@libero.it>
2022-05-31 17:35:18 +0200

    Migration and seeder

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CharactersController extends Controller
+class ComicsController extends Controller
 {
     /**
      * Display a listing of the resource.
