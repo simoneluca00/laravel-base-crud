47174072b40530d772cc7571ce658d0d5572a4a3
simoneluca00 <si.zaino@libero.it>
2022-06-01 18:55:27 +0200

    New files CSS and JS

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!-- link Bootstrap 5 -->
+    {{-- link FontAwesome 6 --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    {{-- link Bootstrap 5 --}}
     <link rel="stylesheet" href=" {{ asset('./css/app.css') }} ">
+
+    {{-- link Custom Style --}}
+    <link rel="stylesheet" href=" {{ asset('./css/style.css') }} ">
     <title>
         @yield('title')
     </title>
@@ -22,7 +28,7 @@
     </div>
 </main>
     
-<!-- script Bootstrap 5 -->
+{{-- script Bootstrap 5 --}}
 <script src=" {{ asset('./js/app.js') }} "></script>
 </body>
 </html>
